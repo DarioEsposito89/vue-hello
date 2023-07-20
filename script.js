3bfcdcc859905034198c8a0234a9f97caaa1356b
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-20 10:30:16 +0200

    Aggiunta immagine

--- script.js
+++ script.js
@@ -4,16 +4,18 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data
 
+
+
 const app = Vue.createApp({
     data() {
         return {
             messaggio: "esercizio di prova",
+            car: "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200",
         };
     },
 
 methods: {
     
-
 }
 });
 
